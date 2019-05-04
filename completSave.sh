a19f13d55c786945267432bb28298bd4d09e8a27
Carl NOMBO <carl.nombo@gmail.com>
2019-05-04 19:22:08 +0100

    Ajout du test de vérification si le fichier de sauvegarde a bien été créé

--- completSave.sh
+++ completSave.sh
@@ -1,1 +1,8 @@
 tar -cvzf /media/carl/B454-40E8/backup`date +'%Y-%m-%d'`.tar.gz  /home/carl/mai$
+if [ -e /media/carl/B454-40E8/backup`date +'%Y-%m-%d'`.tar.gz ]
+then
+        echo “Fichier de sauvegarde cree”;
+else
+        echo “le fichier de sauvegarde n’a pas ete cree”;
+fi
+
